f64a0f69c75ef9d9eeefbfb51531e5fe8b266e5e
BriamGzz <76464991+BriamGzz@users.noreply.github.com>
2022-12-15 12:16:05 -0600

    Pantallitas
    Struct e Index de ADMIN

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,59 @@
+@extends('admin.struct')
+
+@section('Content')
+<div class="col-sm p-3 min-vh-100">
+    <div class="container-fluid">
+        <div class="row">
+            sdasd
+        </div>
+        <div class="row">
+            <div class="">
+                <div class="table-responsive">
+                    <table class="table table-striped-columns" style="text-align-last:center;">
+                        <thead>
+                            <tr>
+                                <th scope="col">Imagen</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Invitado</th>
+                                <th scope="col">Tipo</th>
+                                <th scope="col">Fecha</th>
+                                <th scope="col">Hora Inicio</th>
+                                <th scope="col">Hora Final</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row" ><i class="bi bi bi-0-square-fill fs-1"></i></th>
+                                <td>Evento 0</td>
+                                <td>Jhon Lenon</td>
+                                <td>Conferencia</td>
+                                <td>15/12/22</td>
+                                <td>13:00:00</td>
+                                <td>15:00:00</td>
+                            </tr>
+                            <tr>
+                            <th scope="row" ><i class="bi bi bi-1-square-fill fs-1"></i></th>
+                                <td>Evento 1</td>
+                                <td>Jhon Lenon</td>
+                                <td>Conferencia</td>
+                                <td>15/12/22</td>
+                                <td>13:00:00</td>
+                                <td>15:00:00</td>
+                            </tr>
+                            <tr>
+                            <th scope="row" ><i class="bi bi bi-2-square-fill fs-1"></i></th>
+                                <td>Evento 2</td>
+                                <td>Jhon Lenon</td>
+                                <td>Conferencia</td>
+                                <td>15/12/22</td>
+                                <td>13:00:00</td>
+                                <td>15:00:00</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/admin/struct.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<div class="container-fluid">
+    
+    <div class="row">
+        
+        <div class="col-sm-auto bg-light sticky-top">
+            <div class="d-flex flex-sm-column flex-row flex-nowrap bg-light align-items-center sticky-top">
+                <a href="/" class="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
+                    <!-- AQUI VA EL LOGO!!!!!! -->
+                    <i class="bi-bootstrap fs-1"></i>
+                </a>
+
+                <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center align-items-center">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
+                            <i class="bi bi-calendar2-heart-fill fs-1"></i>
+                            <p>Evento</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+                            <i class="bi bi-people-fill fs-1"></i>
+                            <p>Invitados</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
+                            <i class="bi bi-buildings-fill fs-1"></i>
+                            <p>Empresas</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
+                            <i class="bi bi-mortarboard-fill fs-1"></i>
+                            <p>Maestros</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Customers">
+                            <i class="bi bi-x-circle-fill fs-2"></i>
+                            <p>Salir</p>
+                        </a>
+                    </li>
+                </ul>
+                
+            </div>
+        </div>
+        @yield('Navbar')
+        
+        @yield('Content')
+    </div>
+</div>
+</body>
+</html>
