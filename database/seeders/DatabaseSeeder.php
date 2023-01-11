bc1999f4bae41bb315669dc49ce1a1851f7893d6
BriamGzz <76464991+BriamGzz@users.noreply.github.com>
2023-01-11 12:57:27 -0600

    Descomentar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,43 +17,43 @@ class DatabaseSeeder extends Seeder
     public function run()
     {   
 
-        // //Usuario adminsitrador
-        //  \App\Models\User::create(
-        //      [
-        //      'key' => '546220',
-        //      'password' => 'adminpass123',
-        //      'rol' => 'admin',
-        //      'permanent' => true
-        //      ]
-        // );
+        //Usuario adminsitrador
+         \App\Models\User::create(
+             [
+             'key' => '546220',
+             'password' => 'adminpass123',
+             'rol' => 'admin',
+             'permanent' => true
+             ]
+        );
 
-        // //Usuario staff
-        // \App\Models\User::create(
-        //      [
-        //      'key' => '659854',
-        //      'password' => '659854_staff',
-        //      'rol' => 'staff',
-        //      'permanent' => true
-        //      ]
-        // );
+        //Usuario staff
+        \App\Models\User::create(
+             [
+             'key' => '659854',
+             'password' => '659854_staff',
+             'rol' => 'staff',
+             'permanent' => true
+             ]
+        );
 
 
-        // //Usuario expositor
-        // \App\Models\student::create(
-        //     [
-        //     'enrollment' => '1853806',
-        //     'fullName' => 'Edna Alexandra Lecea Contreras'
-        //     ]
-        // );
+        //Usuario expositor
+        \App\Models\student::create(
+            [
+            'enrollment' => '1853806',
+            'fullName' => 'Edna Alexandra Lecea Contreras'
+            ]
+        );
 
-        // \App\Models\User::create(
-        //     [
-        //     'key' => '1853806',
-        //     'password' => 'leceacontreras_1853806',
-        //     'rol' => 'expositor',
-        //     'permanent' => false
-        //     ]
-        // );
+        \App\Models\User::create(
+            [
+            'key' => '1853806',
+            'password' => 'leceacontreras_1853806',
+            'rol' => 'expositor',
+            'permanent' => false
+            ]
+        );
 
         //Maestro
         \App\Models\User::create(
