33fa2a0f8d9819532e98aa236c85407a00646602
Ritzel2086070 <claudia.hernandezv@uanl.edu.mx>
2023-01-19 13:00:06 -0600

    Pagina Bienvenida avance

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,8 +19,6 @@
         <style>
             /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--tw-bg-opacity: 1;background-color:rgb(255 255 255 / var(--tw-bg-opacity))}.bg-gray-100{--tw-bg-opacity: 1;background-color:rgb(243 244 246 / var(--tw-bg-opacity))}.border-gray-200{--tw-border-opacity: 1;border-color:rgb(229 231 235 / var(--tw-border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{--tw-shadow: 0 1px 3px 0 rgb(0 0 0 / .1), 0 1px 2px -1px rgb(0 0 0 / .1);--tw-shadow-colored: 0 1px 3px 0 var(--tw-shadow-color), 0 1px 2px -1px var(--tw-shadow-color);box-shadow:var(--tw-ring-offset-shadow, 0 0 #0000),var(--tw-ring-shadow, 0 0 #0000),var(--tw-shadow)}.text-center{text-align:center}.text-gray-200{--tw-text-opacity: 1;color:rgb(229 231 235 / var(--tw-text-opacity))}.text-gray-300{--tw-text-opacity: 1;color:rgb(209 213 219 / var(--tw-text-opacity))}.text-gray-400{--tw-text-opacity: 1;color:rgb(156 163 175 / var(--tw-text-opacity))}.text-gray-500{--tw-text-opacity: 1;color:rgb(107 114 128 / var(--tw-text-opacity))}.text-gray-600{--tw-text-opacity: 1;color:rgb(75 85 99 / var(--tw-text-opacity))}.text-gray-700{--tw-text-opacity: 1;color:rgb(55 65 81 / var(--tw-text-opacity))}.text-gray-900{--tw-text-opacity: 1;color:rgb(17 24 39 / var(--tw-text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--tw-bg-opacity: 1;background-color:rgb(31 41 55 / var(--tw-bg-opacity))}.dark\:bg-gray-900{--tw-bg-opacity: 1;background-color:rgb(17 24 39 / var(--tw-bg-opacity))}.dark\:border-gray-700{--tw-border-opacity: 1;border-color:rgb(55 65 81 / var(--tw-border-opacity))}.dark\:text-white{--tw-text-opacity: 1;color:rgb(255 255 255 / var(--tw-text-opacity))}.dark\:text-gray-400{--tw-text-opacity: 1;color:rgb(156 163 175 / var(--tw-text-opacity))}.dark\:text-gray-500{--tw-text-opacity: 1;color:rgb(107 114 128 / var(--tw-text-opacity))}}
         </style>
-
-    
         <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
@@ -46,11 +44,12 @@
 
     </head>
     <body>
-    <nav class="navbar navbar-expand-lg bg-dark sticky-top">
+    <nav class="navbar navbar-expand-lg bg-black sticky-top">
         <div class="container-fluid">
-            
-            <a class="navbar-brand" href="/"> <img class="logo-img" src="{{ asset('images/LOGO.png') }}" height="30"> </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="col-md-0">
+                <a class="m-0 navbar-brand align: center" href="/"> <img class="logo-img" src="{{ asset('images/LOGO.png') }}" height="30"> </a>
+            </div>
+            <button class="m-0 navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
 
@@ -59,25 +58,30 @@
                 <ul class="nav navbar-nav navbar-left">
                     <li class="nav-item">
                         <a class="nav-link active" href="{{route('staffEvento.index')}}">
-                            <p class="m-0 nav-txt"> Eventos </p>
+                            <p class="m-0 btn btn-outline-light"> Registrar Expositores </p>
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link " href="{{route('staffEmpresa.index')}}">
-                            <p class="m-0 nav-txt"> Empresas </p>
+                            <p class="m-0 btn btn-outline-light"> QR Expositor </p>
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link " href="{{route('staffExpositor.index')}}">
-                            <p class="m-0 nav-txt"> Expositor </p>
+                            <p class="m-0 btn btn-outline-light"> Marcar Asistencia </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="{{route('staffExpositor.index')}}">
+                            <p class="m-0 btn btn-outline-light"> Administrar </p>
                         </a>
                     </li>
                 </ul>
 
                 <ul class="nav navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('cerrarSesion') }}">
-                            <p class="m-0 nav-txt"> Salir </p>
+                        <a class="nav-link" href="{{route('inicioSesion.index')}}">
+                            <p class="m-0 btn btn-outline-light"> INICIAR SESION </p>
                         </a>
                     </li>
                 </ul>
@@ -86,14 +90,113 @@
         </div>
     </nav>
         <div class="container-fluid min-vh-100 backgroundImg">
-            <div class="row">
-                <div class="col p-5 ">
-                    <a class="nav-link text-white" href="{{route('inicioSesion.index')}}">INICIAR SESION</a>
-
+            <div class="row align-items-center p-5">
+                <h1 style="text-align: center;"> I n v i t a d o s </h1>
+            </div>
+            <div class="row align-items-center">
+                <div class="col-md-2 col-sm-12 my-5"></div>
+                    <div id="carouselExampleCaptions" class="carousel slide col-md-8" data-bs-ride="carousel">    
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="..." class="d-block w-100" style="text-align: center;" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h2 style="text-align: center;">First slide label</h2>
+                                    <p style="text-align: center;">Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="..." class="d-block w-100" style="text-align: center;" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5 style="text-align: center;">Second slide label</h5>
+                                    <p style="text-align: center;">Some representative placeholder content for the second slide.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="..." class="d-block w-100" style="text-align: center;" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5 style="text-align: center;">Third slide label</h5>
+                                    <p style="text-align: center;">Some representative placeholder content for the third slide.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                    </div>
+                <div class="col-md-3"></div>
+            </div>
+            <div class="row align-items-center p-5">
+                <h1 style="text-align: center;"> M e s a  R e d o n d a </h1>
+            </div>
+            <div class="row align-items-center">
+                <div class="col-md-2 col-sm-12 my-5"></div>
+                    <div id="carouselExampleCaptions" class="carousel slide col-md-8" data-bs-ride="carousel">    
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="..." class="d-block w-100" style="text-align: center;" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h2 style="text-align: center;">First slide label</h2>
+                                    <p style="text-align: center;">Some representative placeholder content for the first slide.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="..." class="d-block w-100" style="text-align: center;" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5 style="text-align: center;">Second slide label</h5>
+                                    <p style="text-align: center;">Some representative placeholder content for the second slide.</p>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img src="..." class="d-block w-100" style="text-align: center;" alt="...">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <h5 style="text-align: center;">Third slide label</h5>
+                                    <p style="text-align: center;">Some representative placeholder content for the third slide.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                    </div>
+                <div class="col-md-3"></div>
+            </div>
+            <div class="row align-items-center p-5">
+                <h1 style="text-align: center;"> E m p r e s a s </h1>
+            </div>
+            <div class="row align-items-center">
+                <div class="col-md-3 col-sm-12 my-5" style="text-align: center;">
+                    <img src="{{ asset('images/camera.png') }}" class="rounded-circle shadow p-3 mb-5" alt="..." height="200">
+                </div>
+                <div class="col-md-3 col-sm-12 my-5" style="text-align: center;">
+                    <img src="{{ asset('images/camera.png') }}" class="rounded-circle shadow p-3 mb-5" alt="..." height="200">
+                </div>
+                <div class="col-md-3 col-sm-12 my-5" style="text-align: center;">
+                    <img src="{{ asset('images/camera.png') }}" class="rounded-circle shadow p-3 mb-5" alt="..." height="200">
+                </div>
+                <div class="col-md-3 col-sm-12 my-5" style="text-align: center;">
+                    <img src="{{ asset('images/camera.png') }}" class="rounded-circle shadow p-3 mb-5" alt="..." height="200">
                 </div>
             </div>
         </div>
-
-
     </body>
 </html>
