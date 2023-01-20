77138a96e5dec8cde8284d55bd4c9e7e7d59ad1f
Ritzel2086070 <claudia.hernandezv@uanl.edu.mx>
2023-01-20 12:29:49 -0600

    Boton logo long in

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -49,9 +49,6 @@
         <div class="container-fluid">
             <div class="col-md-0">
                     <a class="m-0 navbar-brand align: center" href="/"> <img class="logo-img" src="{{ asset('images/LOGO.png') }}" height="30"> </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
             </div>
         </div>
     </nav>
