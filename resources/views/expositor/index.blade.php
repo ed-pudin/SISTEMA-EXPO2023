b1070c3b3bb3117005d7ee7d0f07f55b11ebd330
Ritzel2086070 <claudia.hernandezv@uanl.edu.mx>
2023-02-08 12:12:32 -0600

    Update index.blade.php

--- resources/views/expositor/index.blade.php
+++ resources/views/expositor/index.blade.php
@@ -43,6 +43,17 @@
                             <a href="https://example.com">Networking</a>
                         </div>
 
+                        <div class="row d-md-flex justify-content-center align-items-center" style="margin-top:10px">
+                        <div class="col-1 d-md-flex justify-content-center align-items-center" style="margin-top:1px">
+                            <button type="button" class="btn btn-primary" onClick="window.location.reload();">
+                            <svg width="20" height="20" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                            </svg>
+                            </button>
+                        </div>
+                        </div>
+
                         @if(count($projects) > 0)
                         <div class="col-12 d-md-flex justify-content-center align-items-center" style="margin-top:20px">
                             <hr class="colorfull col-md-11">
