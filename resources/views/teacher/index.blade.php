0b9703e9d33b73538e81fef5c717d1abc1f56f39
Nevi63 <cdpinkus@gmail.com>
2023-06-05 11:45:36 -0600

    Planes
    Planes 420 y 440

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -48,7 +48,16 @@
             <!--DROP DOWNS-->
 
             <div class="d-flex justify-content-center flex-wrap">
-                <div class="col-12 col-md-3 col-lg-2 col-xl-1 m-2">
+            <div class="col-12 col-md-3 col-lg-2 col-xl-1 m-2">
+                    <div class="form-floating">
+                        <select class="form-select" id="planCmb" onchange=updateInputSemesterCmb()>
+
+                        </select>
+                        <label for="planCmb">Plan</label>
+                    </div>
+                </div>   
+
+            <div class="col-12 col-md-6 col-lg-4 col-xl-1 m-2">
                     <div class="form-floating">
                         <select class="form-select" id="semesterCmb" onchange=updateInputSemesterCmb()>
 
@@ -57,9 +66,9 @@
                     </div>
                 </div>
 
-                <div class="col-12 col-md-6 col-lg-4 col-xl-3 m-2">
+                <div class="col-12 col-md-9 col-lg-6 col-xl-3 m-2">
                     <div class="form-floating">
-                        <select class="form-select" id="UACmb">
+                        <select class="form-select" id="UACmb" onchange="document.getElementById('UA').value = document.getElementById('UACmb').value;">
 
                         </select>
                         <label for="UACmb">Materia</label>
@@ -79,8 +88,9 @@
                 @csrf
                     <!--HIDDEN INPUTS-->
 
-                    <input type="text" name="semester" id="semester" hidden>
-                    <input type="text" name="UA" id="UA" hidden>
+                    <input type="text" name="semester" id="semester" value="document.getElementById('semesterCmb').value;" hidden>
+                    <input type="text" name="plan" id="plan" hidden>
+                    <input type="text" name="UA" id="UA" hidden required>
                     <input type="text" id="inputCount" name="inputCount" value=1 hidden>
 
                     <div class="d-flex justify-content-center flex-wrap">
@@ -161,10 +171,31 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+      function updateInputPlanCmb(){
+        var sel=document.getElementById("planCmb");
+
+        for(var planv in Plans){
+          // Crear una nueva opción
+          var opcion = document.createElement("option");
+          opcion.value =Plans[planv];
+          opcion.text =Plans[planv];
+
+          // Agregar la opción al select
+          sel.add(opcion);
+
+        }
+          //Se updatea el valor en los hidden inputs
+         document.getElementById("plan").value = document.getElementById("planCmb").value;
+    }
+    var Plans = new Array('420','440');
+    updateInputPlanCmb();
+    </script>
+
     <script type="text/javascript">
 
         function updateInputSemesterCmb(){
-
+            document.getElementById('plan').value = document.getElementById('planCmb').value;
             var select = document.getElementById("UACmb");
 
             //Eliminar opciones
@@ -172,7 +203,8 @@
 
             var optionSelected = document.getElementById("semesterCmb").value;
 
-            for(var subject in semestersSubjects[optionSelected]){
+          if (document.getElementById("planCmb").value=='420') {
+             for(var subject in semestersSubjects[optionSelected]){
                 // Crear una nueva opción
                 var opcion = document.createElement("option");
 
@@ -183,7 +215,19 @@
                 select.add(opcion);
 
             }
+          } else {
+             for(var subject in semestersSubjects2[optionSelected]){
+                // Crear una nueva opción
+                var opcion = document.createElement("option");
 
+                opcion.value = semestersSubjects2[optionSelected][subject];
+                opcion.text = semestersSubjects2[optionSelected][subject];
+
+                // Agregar la opción al select
+                select.add(opcion);
+            }
+
+            }
              //Se updatea el valor en los hidden inputs
              document.getElementById("semester").value = document.getElementById("semesterCmb").value;
              document.getElementById("UA").value = document.getElementById("UACmb").value;
@@ -192,15 +236,28 @@
 
         var semestersSubjects =
         {
-        '2': ['Programación básica','Matemáticas para videojuegos I', 'Tecnologías multimedia', 'Dibujo de la anatomía humana'],
-        '3': ['Programación avanzada', 'Matemáticas para videojuegos II', 'Producción multimedia', 'Modelado arquitectónico'],
-        '4': ['Modelado orgánico', 'Lenguaje ensamblador'],
-        '5': ['Gráficas computacionales I', 'Fotografía digital', 'Cinematografía', 'Animación básica', 'Preproducción de vídeo', 'Preproducción 2D', 'Administración de alto volumen de datos'],
-        '6': ['Programación web I', 'Gráficas computacionales II', 'Escenarios de videojuegos', 'Modelado en alto poligonaje', 'Interface y experiencia de usuario en web', 'Ilustración digital', 'Efectos Visuales I'],
-        '7': ['Programación web de capa intermedia', 'Programación orientada a internet', '	Optimización de videojuegos', 'Gráficas computacionales en web', 'Base de datos multimedia', 'Actuación y dirección para animación', 'Animación tradicional de humanos y de animales', 'Efectos visuales II', '	Programación de sistemas móviles'],
-        '8': ['Diseño de videojuegos en línea', 'Programación web II', 'Realidad Virtual', 'Animación tradicional de escenarios', 'Esqueletos de personajes', 'Iluminación y audio'],
-        '9': ['Postproducción']
+        '2': ['Tecnologías multimedia', 'Dibujo de la anatomía humana'],
+        '3': ['Producción multimedia', 'Modelado arquitectónico'],
+        '4': ['Modelado orgánico'],
+        '5': ['Administración de alto volumen de datos','Diseño de hápticos','Modelos de administración de datos','Animación básica','Fotografía digital','Gráficas computacionales I','Preproducción 2D','Preproducción de video'],
+        '6': ['Efectos Visuales I','Escenarios de videojuegos','Gráficas computacionales II','Ilustración digital','Modelado en alto poligonaje'],
+        '7': ['Base de datos multimedia','Actuación y dirección para animación','Animación tradicional de humanos y de animales','Efectos visuales II','Optimización de videojuegos','Programación de sistemas móviles'],
+        '8': ['Iluminación y audio','Realidad virtual','Animación tradicional de escenarios','Diseños de videojuegos en linea','Esqueletos de personajes'],
+        '9': ['Postproducción'],
+        '10':[]
         }
+        var semestersSubjects2 =
+        {
+        '2': ['Cultura de paz y derechos humanos','Ética, transparencia y cultura de la legalidad','Igualdad de género, diversidad sexual e inclusión','Tópicos de álgebra','Cálculo integral','Mecánica traslacional y rotacional','Programación básica'],
+        '3': ['Programación estructurada','Relaciones espaciales para videojuegos','Producción multimedia','Fundamentos del dibujo artístico','Metodologías ágiles de trabajo','Proyección de negocios tecnológicos y animaciones','Modelado arquitectónico','Laboratorio de programación estructurada'],
+        '4': ['Programación avanzada','Transformaciones gráficas para videojuegos','Fundamentos de los videojuegos','Tecnologías multimedia','Fotografía digital','Modelo de administración de datos'],
+        '5': ['Interfaz y experiencia de usuario','Programación en entornos bidimensionales','Lógica digital','Producción de guiones','Iluminación y audio','Programación orientada a objetos','Laboratorio de programación orientada a objetos','Modelado en alto poligonaje','Preproducción para series animadas','Programación en Android'],
+        '6': ['Programación en entornos tridimensionales','Diseño de hápticos','Redes computacionales','Escenarios de videojuegos','Fundamentos y producción cinematográfica','Laboratorio de redes computacionales','Esqueletos de personajes','Diseño de personajes para animación','Programación para sistema operativo de internet','Control de calidad en software','Modelado tridimensional','Ilustración digital'],
+        '7': ['Gráficas computacionales','Procesamiento de imágenes','Optimización de videojuegos','Programación de sitios web','Introducción a efectos visuales','Administración de proyectos multimedia','Laboratorio de programación de sitios web','Animación por computadora','Principios de la animación tradicional','Ingeniería de software'],
+        '8': ['Interfaces y aplicaciones web','Escenarios digitales','Gestión y producción de efectos visuales','Realidad virtual y aumentada','Diseño de videojuegos en línea','Propiedad intelectual','Laboratorio de interfaces y aplicaciones web','Actuación y dirección para animación','Animación tradicional avanzada','Ingeniería en la nube'],
+        '9': ['Gráficas computacionales en web','Inglés para tecnologías','Investigación y desarrollo de proyectos','Mercadotecnia','Programación multiplataforma','Laboratorios de gráficas computaciones en web','Texturizado de la nueva generación','Animación por marionetas','Programación de algoritmos avanzados'],
+        '10': ['Seminario para el desempeño profesional','Servicio social','Postproducción para series animadas','Postproducción para entornos virtuales','Inteligencia artificial y ciencia de datos']   
+    }
 
         var select = document.getElementById("semesterCmb");
 
